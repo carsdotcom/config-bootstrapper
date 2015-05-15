2d4797a4ce6c2a9e8c08d1400f91a7d1fb403eb6
Mac Heller-Ogden <mac@machellerogden.com>
2015-05-15 10:27:14 -0500

    updated to work with node

--- src/config-bootstrapper.js
+++ src/config-bootstrapper.js
@@ -1,5 +1,5 @@
 /*!
- * Config Bootstrapper v0.0.0 <https://github.com/carsdotcom>
+ * Config Bootstrapper v0.0.1 <https://github.com/carsdotcom>
  * @license Apache 2.0
  * @copyright 2015 Cars.com <http://www.cars.com/>
  * @author Mac Heller-Ogden <mheller-ogden@cars.com>
@@ -10,40 +10,56 @@
     else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);
     else (function () { return this || (0, eval)('this'); }())[name] = definition();
 }('ConfigBootstrapper', function () {
+    var NodeLocalStorage;
+
+    if (typeof localStorage === 'undefined' || localStorage === null) {
+        /* eslint no-undef: 0 */
+        NodeLocalStorage = require('node-localstorage').LocalStorage;
+        localStorage = new NodeLocalStorage('./localstorage-temp-data');
+    }
+
+    if (typeof XMLHttpRequest === 'undefined' || XMLHttpRequest === null) {
+        /* eslint no-undef: 0 */
+        XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+    }
 
     function ConfigBootstrapper(options) {
+        options = (typeof options == "object") ? options : {};
+        options.refreshRate = options.refreshRate || 300;
+        options.dataStorageKey = options.dataStorageKey || 'configBootstrapper.data';
+        options.timestampStorageKey = options.timestampStorageKey || 'configBootstrapper.timestamp';
         this.options = options;
     }
 
     ConfigBootstrapper.prototype.ready = function (callback) {
-        var configBootstrapper = this;
-        this._getJson(this.options.baseUrl, callback);
+        var cbs = this;
+        this._getJson(this.options.url, callback);
 
         setTimeout(function refreshHandler() {
-            configBootstrapper._getJson(configBootstrapper.options.baseUrl, function () {});
-            setTimeout(refreshHandler, configBootstrapper.options.refreshRate);
+            cbs._getJson(cbs.options.url, function () {});
+            setTimeout(refreshHandler, cbs.options.refreshRate);
         }, this.options.refreshRate);
     };
 
     ConfigBootstrapper.prototype._getJson = function(url, callback) {
-        var bootstrap,
+        var cbs,
             ts,
             now,
             xhr;
 
-        bootstrap = this;
+        cbs = this;
 
-        ts = +window.localStorage.getItem(bootstrap.options.timestampStorageKey);
+        ts = parseInt(localStorage.getItem(cbs.options.timestampStorageKey), 10);
         now = Date.now();
 
-        if (!ts || (now >= (ts + bootstrap.options.refreshRate * 1000))) {
+        if (!ts || (now >= (ts + cbs.options.refreshRate * 1000))) {
             xhr = new XMLHttpRequest();
             xhr.open('GET', url, true);
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4) { // `DONE`
                     if (xhr.status === 200) {
-                        window.localStorage.setItem(bootstrap.options.dataStorageKey, xhr.responseText);
-                        window.localStorage.setItem(bootstrap.options.timestampStorageKey, now);
+                        localStorage.setItem(cbs.options.dataStorageKey, xhr.responseText);
+                        localStorage.setItem(cbs.options.timestampStorageKey, now);
                     }
                     callback();
                 }
--- test/specs/config-bootstrapper.js
+++ test/specs/config-bootstrapper.js
@@ -15,17 +15,18 @@ describe("Loading config bootstrapper", function() {
     });
 
     options = {
-        baseUrl: "http://static-config.cars.example.com",
+        url: 'http://static-config.cars.example.com',
         refreshRate: 300,
-        dataStorageKey: 'cars.configData'
+        dataStorageKey: 'cars.configData',
+        timestampStorageKey: 'cars.configTimestamp'
     };
 
     beforeEach(function () {
         var data = {};
-        spyOn(window.localStorage, 'setItem').and.callFake(function(key, val){
+        spyOn(localStorage, 'setItem').and.callFake(function(key, val){
             data[key] = val;
         });
-        spyOn(window.localStorage, 'getItem').and.callFake(function(key){
+        spyOn(localStorage, 'getItem').and.callFake(function(key){
             return data[key];
         });
     });
@@ -40,72 +41,72 @@ describe("Loading config bootstrapper", function() {
             };
         });
 
-        describe('given a 5 minute refresh rate', function () {
-            describe('when a timestamp exists and is less than or equal to 5 minutes old', function () {
+        describe("given a 5 minute refresh rate", function () {
+            describe("when a timestamp exists and is less than or equal to 5 minutes old", function () {
                 it("should not load the config from the api", function () {
                     var callback,
                         request;
 
-                    spyOn(window.Date, 'now').and.returnValue(timeValues.now + timeValues.almostFiveMinutes);
+                    spyOn(Date, 'now').and.returnValue(timeValues.now + timeValues.almostFiveMinutes);
 
-                    window.localStorage.setItem(options.dataStorageKey, JSON.stringify({ myFlag: true }));
-                    window.localStorage.setItem(options.timestampStorageKey, timeValues.now);
+                    localStorage.setItem(options.dataStorageKey, JSON.stringify({ myFlag: true }));
+                    localStorage.setItem(options.timestampStorageKey, timeValues.now);
 
-                    callback = jasmine.createSpy("callback");
+                    callback = jasmine.createSpy('callback');
                     configBootstrapper.ready(callback);
                     request = jasmine.Ajax.requests.mostRecent();
 
                     expect(request).toBeUndefined();
-                    expect(JSON.parse(window.localStorage.getItem(options.dataStorageKey))).toEqual({ myFlag: true });
+                    expect(JSON.parse(localStorage.getItem(options.dataStorageKey))).toEqual({ myFlag: true });
                 });
             });
 
-            describe('when a timestamp exists and is older than 5 minutes old', function () {
+            describe("when a timestamp exists and is older than 5 minutes old", function () {
                 it("should load the config from the api", function () {
                     var callback,
                         request;
 
-                    spyOn(window.Date, 'now').and.returnValue(timeValues.now + timeValues.almostFiveMinutes);
+                    spyOn(Date, 'now').and.returnValue(timeValues.now + timeValues.almostFiveMinutes);
 
-                    window.localStorage.setItem(options.dataStorageKey, JSON.stringify({ myFlag: true }));
-                    window.localStorage.setItem(options.timestampStorageKey, timeValues.now - 1);
+                    localStorage.setItem(options.dataStorageKey, JSON.stringify({ myFlag: true }));
+                    localStorage.setItem(options.timestampStorageKey, timeValues.now - 1);
 
-                    callback = jasmine.createSpy("callback");
+                    callback = jasmine.createSpy('callback');
                     configBootstrapper.ready(callback);
                     request = jasmine.Ajax.requests.mostRecent();
                     request.respondWith({ status: 200, responseText: JSON.stringify({ myFlag: false }) });
 
                     expect(callback).toHaveBeenCalled();
-                    expect(JSON.parse(window.localStorage.getItem(options.dataStorageKey))).toEqual({ myFlag: false });
+                    expect(JSON.parse(localStorage.getItem(options.dataStorageKey))).toEqual({ myFlag: false });
                 });
             });
 
-            describe('when a timestamp does not exist or if the timestamp is older than 5 minutes', function () {
+            describe("when a timestamp does not exist or if the timestamp is older than 5 minutes", function () {
 
                 describe("when the GET request is successful", function () {
                     it("loads the config from the api", function () {
                         var callback,
                             request;
 
-                        callback = jasmine.createSpy("callback");
+                        callback = jasmine.createSpy('callback');
                         configBootstrapper.ready(callback);
 
                         expect(callback).not.toHaveBeenCalled();
 
                         request = jasmine.Ajax.requests.mostRecent();
 
-                        expect(request.url).toBe("http://static-config.cars.example.com");
-                        expect(request.method).toBe("GET");
+                        expect(request.url).toBe('http://static-config.cars.example.com');
+                        expect(request.method).toBe('GET');
 
                         request.respondWith({ status: 200, responseText: JSON.stringify({ myFlag: true }) });
 
                         expect(callback).toHaveBeenCalled();
-                        expect(JSON.parse(window.localStorage.getItem(options.dataStorageKey))).toEqual({ myFlag: true });
+                        expect(JSON.parse(localStorage.getItem(options.dataStorageKey))).toEqual({ myFlag: true });
                     });
 
                 });
 
-                describe('when the GET request fails', function () {
+                describe("when the GET request fails", function () {
                     it("the existing configuration data in local storage does not get overwritten", function () {
                         var callback, request;
 
@@ -114,7 +115,7 @@ describe("Loading config bootstrapper", function() {
 
                         expect(callback).not.toHaveBeenCalled();
 
-                        window.localStorage.setItem(options.dataStorageKey, JSON.stringify({ myFlag: true }));
+                        localStorage.setItem(options.dataStorageKey, JSON.stringify({ myFlag: true }));
                         request = jasmine.Ajax.requests.mostRecent();
 
                         expect(request.url).toBe('http://static-config.cars.example.com');
@@ -123,7 +124,7 @@ describe("Loading config bootstrapper", function() {
                         request.respondWith({ status: 500, responseText:""});
 
                         expect(callback).toHaveBeenCalled();
-                        expect(JSON.parse(window.localStorage.getItem(options.dataStorageKey))).toEqual({ myFlag: true });
+                        expect(JSON.parse(localStorage.getItem(options.dataStorageKey))).toEqual({ myFlag: true });
                     });
                 });
             });
@@ -132,7 +133,7 @@ describe("Loading config bootstrapper", function() {
                 it("every 5 minutes config data will be refreshed from the server", function () {
                     var request;
 
-                    spyOn(window.Date, 'now').and.callFake((function () {
+                    spyOn(Date, 'now').and.callFake((function () {
                         var now = timeValues.now;
                         return function () {
                             now = now + options.refreshRate * 1000;
@@ -145,7 +146,7 @@ describe("Loading config bootstrapper", function() {
                     request = jasmine.Ajax.requests.mostRecent();
                     request.respondWith({ status: 200, responseText: JSON.stringify({ myFlag: true }) });
 
-                    expect(JSON.parse(window.localStorage.getItem(options.dataStorageKey))).toEqual({ myFlag: true });
+                    expect(JSON.parse(localStorage.getItem(options.dataStorageKey))).toEqual({ myFlag: true });
 
                     jasmine.clock().tick(options.refreshRate - 1);
 
@@ -159,14 +160,14 @@ describe("Loading config bootstrapper", function() {
                     request = jasmine.Ajax.requests.mostRecent();
                     request.respondWith({ status: 200, responseText: JSON.stringify({ myNewFlag: true }) });
 
-                    expect(JSON.parse(window.localStorage.getItem(options.dataStorageKey))).toEqual({ myNewFlag: true });
+                    expect(JSON.parse(localStorage.getItem(options.dataStorageKey))).toEqual({ myNewFlag: true });
 
                     jasmine.clock().tick(options.refreshRate);
 
                     request = jasmine.Ajax.requests.mostRecent();
                     request.respondWith({ status: 200, responseText: JSON.stringify({ myNewestFlag: true }) });
 
-                    expect(JSON.parse(window.localStorage.getItem(options.dataStorageKey))).toEqual({ myNewestFlag: true });
+                    expect(JSON.parse(localStorage.getItem(options.dataStorageKey))).toEqual({ myNewestFlag: true });
                 });
             });
         });
